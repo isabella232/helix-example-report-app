5f688a973235b4b7e468a9ed95e5be1e959c7d10
Claudio Bianucci <claudio.bianucci@namics.com>
2018-12-19 09:51:59 +0100

    Introduced timeline library.

--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -1,5 +1,54 @@
 const React = require('react');
 const { Container, Row, Col } = require('reactstrap');
+const Timeline = require('react-image-timeline-sixteen').default;
+require('../../node_modules/react-image-timeline-sixteen/dist/timeline.css');
+
+const R = require('ramda');
+const moment = require('moment');
+
+// eslint-disable-next-line
+const ipsum = 'Sed leo elit, pellentesque sit amet congue quis, ornare nec lorem. Phasellus tincidunt rhoncus magna, eget elementum odio rutrum fermentum. Ut a justo lacus. Maecenas blandit molestie felis ac viverra. Pellentesque sagittis ligula neque, sit amet feugiat massa tempor sed. Duis id bibendum ex, pulvinar tincidunt orci. Curabitur id sem urna. Maecenas sed elit malesuada, cursus ligula ut, vestibulum lorem. Suspendisse vitae ultrices libero. Mauris maximus, ligula vitae tincidunt scelerisque, ipsum magna euismod massa, eu vulputate enim est tempus sem. Maecenas id nibh vitae ante volutpat facilisis nec eget velit. Proin et ligula feugiat, auctor tellus sit amet, accumsan neque. Quisque porttitor lectus quis elit fermentum, a facilisis est suscipit. Integer consectetur dapibus nisi, ut lacinia enim vulputate vitae. Curabitur id diam mauris. Duis dictum, dolor at porttitor aliquet, justo libero mattis magna, eu pellentesque augue mauris eget erat. Pellentesque lacinia velit nec ullamcorper mollis. Pellentesque lacus tortor, maximus eget tincidunt non, luctus scelerisque odio. Suspendisse potenti. Etiam vel augue blandit, auctor sem sit amet, imperdiet dolor. Ut a quam laoreet, feugiat orci sed, feugiat nulla. Nulla gravida nisi eu ex egestasdapibus.';
+
+const cities = [
+  {
+    title: 'Berlin, Germany',
+    imageUrl: 'https://s3.amazonaws.com/aaron-cdn/react-image-timeline/berlin.jpg',
+  },
+  {
+    title: 'Chicago, Illinois',
+    imageUrl: 'https://s3.amazonaws.com/aaron-cdn/react-image-timeline/chicago.jpg',
+  },
+  {
+    title: 'Cairo, Egypt',
+    imageUrl: 'https://s3.amazonaws.com/aaron-cdn/react-image-timeline/egypt.jpg',
+  },
+  {
+    title: 'London, England',
+    imageUrl: 'https://s3.amazonaws.com/aaron-cdn/react-image-timeline/london.jpg',
+  },
+];
+
+function randomLengthText() {
+  const minLength = 50;
+  return ipsum.slice(0, Math.random() * (R.length(ipsum) - minLength) + minLength);
+}
+
+function getSampleData() {
+  let offset = 0;
+  const orderedCities = R.map((city) => {
+    offset += Math.random() * 100;
+    return R.merge({
+      date: moment('2013-02-08').add(offset, 'days'),
+      text: randomLengthText(),
+      buttonText: 'Read More',
+      onClick: () => {},
+      extras: {
+        foo: '#Travel',
+      },
+    }, city);
+  }, cities);
+  return orderedCities;
+}
 
 class TimeLine extends React.Component {
 
@@ -22,11 +71,7 @@ class TimeLine extends React.Component {
       <Container>
         <Row>
           <Col>
-            <div className="timeline">
-              <h4>TimeLine</h4>
-              <button onClick={this.handleClick}>Click Me</button>
-              <span>{this.state.count}</span>
-            </div>
+            <Timeline events={getSampleData(true)} />
           </Col>
         </Row>
       </Container>
